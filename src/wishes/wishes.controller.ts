27a8e76e34a43c5d551f04b9772aa97dd92c2a79
Elena Soloveva <seaver@yandex.ru>
2022-12-22 09:35:03 +0200

    Fix: add updateWish functionality

--- src/wishes/wishes.controller.ts
+++ src/wishes/wishes.controller.ts
@@ -1,8 +1,19 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { WishesService } from './wishes.service';
 import { WishEntity } from './wish.entity';
 import { CreateWishDto } from './dto/createWishDto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from '../auth/decorators/getUser.decorator';
+import { UserEntity } from '../users/user.entity';
+import { UpdateWishDto } from './dto/updateWishDto';
 
 @Controller('wishes')
 export class WishesController {
@@ -25,12 +36,22 @@ export class WishesController {
   @Get(':id')
   @UseGuards(AuthGuard('jwt'))
   getWishById(@Param('id') id: string): Promise<WishEntity> {
-    return this.wishesService.getWishById(id);
+    return this.wishesService.getWishById(Number.parseInt(id));
   }
 
   @Post()
   @UseGuards(AuthGuard('jwt'))
   createWish(@Body() createWishDto: CreateWishDto): Promise<WishEntity> {
     return this.wishesService.createWish(createWishDto);
+  }
+
+  @Patch(':id')
+  @UseGuards(AuthGuard('jwt'))
+  updateWish(
+    @Param('id') id: number,
+    @GetUser() user: UserEntity,
+    @Body() updateWishDto: UpdateWishDto,
+  ): Promise<WishEntity> {
+    return this.wishesService.updateWish(id, user, updateWishDto);
   }
 }
--- src/wishes/wishes.service.ts
+++ src/wishes/wishes.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { WishEntity } from './wish.entity';
 import { CreateWishDto } from './dto/createWishDto';
+import { UserEntity } from '../users/user.entity';
+import { UpdateWishDto } from './dto/updateWishDto';
 
 @Injectable()
 export class WishesService {
@@ -41,7 +48,27 @@ export class WishesService {
       take: Number(20),
     });
   }
-  getWishById(id): Promise<WishEntity> {
+  getWishById(id: number): Promise<WishEntity> {
     return this.wishesRepository.findOneBy({ id });
+  }
+  async updateWish(
+    id: number,
+    user: UserEntity,
+    updateWishDto: UpdateWishDto,
+  ): Promise<WishEntity> {
+    const wish = await this.getWishById(id);
+    if (!wish) {
+      throw new NotFoundException(`Wish with id ${id} does not exist`);
+    } else if (wish.owner.id !== user.id) {
+      throw new UnauthorizedException('You can update only your own wishes');
+    } else {
+      try {
+        await this.wishesRepository.update({ id }, updateWishDto);
+        return this.getWishById(id);
+      } catch (err) {
+        console.log(err);
+        throw new BadRequestException(`${err.detail}`);
+      }
+    }
   }
 }
