f12ebc04d12f9c916d75c2aa1a862f6e37ec4c5f
Elena Soloveva <seaver@yandex.ru>
2022-12-25 13:02:24 +0200

    Feat: add wishlists service and controller

--- src/wishlists/wishlists.module.ts
+++ src/wishlists/wishlists.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WishListEntity } from './wishList.entity';
+import { WishlistsController } from './wishlists.controller';
+import { WishlistsService } from './wishlists.service';
 
-@Module({ imports: [TypeOrmModule.forFeature([WishListEntity])] })
+@Module({
+  imports: [TypeOrmModule.forFeature([WishListEntity])],
+  controllers: [WishlistsController],
+  providers: [WishlistsService],
+})
 export class WishlistsModule {}
